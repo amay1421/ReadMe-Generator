33acfc5c334d80b0d3257f6b5c42e9f9df5e0f66
Alyson May <amay1421@yahoo.com>
2021-01-24 17:55:40 -0600

    added more questions

--- index.js
+++ index.js
@@ -13,9 +13,55 @@ const questions = [
     },
     {
         type: "input",
-        name: "userAge",
-        message: "What is your age?"
-    }
+        name: "repository",
+        message: "What is your name of your GitHub repository?"
+    },
+    {
+        type: "input",
+        name: "repoTitle",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        description: "questions",
+        message: "What technology was used?",
+    
+    },
+    {
+        type: "input",
+        description: "usage",
+        message: "What is the project usage for?",
+    
+    },
+    {
+        type: "input",
+        description: "descrption",
+        message: "How would you describe this project?"
+    },
+    {
+        type: "input",
+        description: "installation",
+        message: "What steps are required for installation?"
+    },
+    {
+        type: "input",
+        description: "contribution",
+        message: "How can other developers contribute to your project?"
+    },
+    {
+        type: "list",
+        description: "licensing",
+        message: "What licensing would you like for your project?",
+        choices: []
+    },
+    {
+        type: "input",
+        description: "questions",
+        message: "What is your email address for any future inquiries?",
+    
+    },
+    
+   
 ];
 
 inquirer.prompt(questions).then((answer) => {
